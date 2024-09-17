54101413f2f6e9a08e16487bd44b9a932ba3ceb7
bubu20061989 <121917471+bubu20061989@users.noreply.github.com>
2024-09-17 18:31:26 +0700

    thầy viên

--- adminProject/member/models.py
+++ adminProject/member/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User  # Assuming you use Django's built-in User model
+
 
 class Employee(models.Model):
     STATUS_CHOICES = [
@@ -43,3 +45,21 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.product_id} - {self.type}'
+    
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart for {self.user.username}"
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('cart', 'product')
+
+    def __str__(self):
+        return f"{self.quantity} of {self.product.product_id} in cart"
